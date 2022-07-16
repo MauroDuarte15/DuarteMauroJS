5bafc05d4f6718a9ff31f63c5eec57e3379324f9
MauroDuarte15 <mauroleonelduarte.507arg@gmail.com>
2022-07-16 20:47:29 -0300

    fetch

--- js/logica.js
+++ js/logica.js
@@ -2,6 +2,8 @@ const Clickbutton = document.querySelectorAll('.button')
 const tbody = document.querySelector('.tbody')
 
 let carrito = []
+const URL = `js/articulos.json`
+const contenidoDOM = document.querySelector("#contenido")
 
 Clickbutton.forEach(btn => {
   btn.addEventListener('click', agregarAlCarrito)
@@ -16,7 +18,52 @@ btn_comprar.addEventListener('click', () => {
 })
 })
 
+const obtenerContenido = async (URL)=> {
+  let cardsAmostrar = ""
+      try {
+        const response = await fetch(URL)
+        const data = await response.json()
+              data.forEach(contenido => {
+                 cardsAmostrar += retornoCardContenido(contenido)
+              })
+              contenidoDOM.innerHTML = cardsAmostrar
+      } catch (error) {
+              contenidoDOM.innerHTML = retornoCardError()
+      } finally {
+              cargandoDOM.innerHTML = ""
+      }
+}
 
+const retornoCardContenido = (contenido)=> {
+  const {imagen, nombre, precio} = contenido
+
+  return `<div class="col d-flex justify-content-center mb-4">
+                    <div class="card shadow mb-1 bg-dark rounded" style="width: 20rem;">
+                        <h5 class="card-title pt-2 text-center text-primary">${nombre}</h5>
+                        <img src="${imagen}" class="card-img-top img_main" alt="...">
+                        <div class="card-body">
+                            <p class="card-text text-white-50 description">Some quick example text to build on the
+                                card title and make up the bulk of the card's content.</p>
+                            <h5 class="text-primary">Precio: <span class="precio">${precio}</span></h5>
+                            <div class="d-grid gap-2">
+                                <button class="btn btn-primary button">Añadir al carrito</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+}
+const retornoCardError = ()=> {
+  return `<div class="center white-text"> 
+              <br><br><br><br> 
+              <h4>El contenido parece no estar disponible. Intente nuevamente en unos minutos.</h4> 
+          </div>`
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+  setTimeout(() => {
+     obtenerContenido(URL)      
+  }, 2000);
+})
 
 function agregarAlCarrito(e){
   const button = e.target
